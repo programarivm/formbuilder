2d6f784c5eae065f17a5f01a63dca87b4c9da2ce
programarivm <info@programarivm.com>
2021-01-13 14:44:22 +0100

    Installed the Dashboard template

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Dashboard from './Dashboard';
 import reportWebVitals from './reportWebVitals';
 
+/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+); */
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Dashboard />
   </React.StrictMode>,
   document.getElementById('root')
 );
