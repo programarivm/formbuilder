8e1c387f2e33ec7d0f4c438509ae7e7f3c5edeae
programarivm <info@programarivm.com>
2021-01-14 10:20:33 +0100

    Structured the app into folders

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import Dashboard from './Dashboard';
-import reportWebVitals from './reportWebVitals';
-
-/*
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-); */
+import Dashboard from './components/Dashboard';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,8 +8,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
