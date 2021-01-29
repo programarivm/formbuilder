f1b40ad58b761304176df8013a0f58153b8784b2
programarivm <info@programarivm.com>
2021-01-29 09:16:12 +0100

    Converted HTML code to React components

--- src/components/Dnd/CardContent/Select.js
+++ src/components/Dnd/CardContent/Select.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { CardContent } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
+var HtmlToReactParser = require('html-to-react').Parser;
 
 const Select = ({ elem }) => {
+  const htmlToReactParser = new HtmlToReactParser();
+  const reactElement = htmlToReactParser.parse(elem.html);
+
   return (
     <CardContent>
       <Typography gutterBottom variant="h6">
         Select
       </Typography>
-      {elem.html}
+      {reactElement}
     </CardContent>
   );
 };
--- src/components/Dnd/CardContent/Textarea.js
+++ src/components/Dnd/CardContent/Textarea.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { CardContent } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
+var HtmlToReactParser = require('html-to-react').Parser;
 
 const Textarea = ({ elem }) => {
+  const htmlToReactParser = new HtmlToReactParser();
+  const reactElement = htmlToReactParser.parse(elem.html);
+
   return (
     <CardContent>
       <Typography gutterBottom variant="h6">
         Textarea
       </Typography>
-      {elem.html}
+      {reactElement}
     </CardContent>
   );
 };
