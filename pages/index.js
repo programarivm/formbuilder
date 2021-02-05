3650bc81737afc27a413f02161a32e39f6b7d49e
programarivm <info@programarivm.com>
2021-02-05 16:13:27 +0100

    Designed the index page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,74 @@
-import { Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
+import { Box, Container, Grid, Paper, Typography } from '@material-ui/core'
 import MainNav from 'components/MainNav'
+import Footer from 'components/Footer'
+import Head from 'next/head'
+
+const useStyles = makeStyles((theme) => ({
+  box: {
+    padding: theme.spacing(2),
+    margin: theme.spacing(2),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    color: theme.palette.text.secondary,
+  },
+}));
 
 const About = () => {
-  return <>
+  const classes = useStyles()
+
+  return <div>
+    <Head>
+      <title>HTML Form Builder</title>
+      <meta name="description" content="HTML form builder to design nice forms with no code." />
+    </Head>
     <MainNav />
     <Container maxWidth="lg">
-      <h1>About</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <Box className={classes.box}>
+        <Typography variant="h3" component="h1" align="center" gutterBottom>
+          It's a piece of cake!
+        </Typography>
+        <Typography variant="h4" component="h2" align="center" gutterBottom>
+          Build HTML forms without writing any code at all
+        </Typography>
+        <Typography variant="h5" component="h3" align="center">
+          Just drag and drop with this absolutely amazing web app
+        </Typography>
+      </Box>
+      <Grid container spacing={5}>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>
+            <Typography variant="body1">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>
+            <Typography variant="body1">
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>
+            <Typography variant="body1">
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>
+            <Typography variant="body1">
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Typography>
+          </Paper>
+        </Grid>
+      </Grid>
     </Container>
-  </>
+    <Footer />
+  </div>
 }
 
 export default About
---
+++ src/components/Footer.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Grid, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import EmojiFoodBeverage from '@material-ui/icons/EmojiFoodBeverageTwoTone';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import LockIcon from '@material-ui/icons/LockTwoTone';
+import SubjectIcon from '@material-ui/icons/SubjectTwoTone';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import Typography from '@material-ui/core/Typography';
+
+const Footer = () => {
+  return (
+    <Grid container style={{
+        marginTop: 90,
+        paddingTop: 35,
+        paddingBottom: 35,
+        backgroundColor: '#e8e8e8'
+      }}
+    >
+      <Grid container item xs={6} style={{ paddingTop: 35 }}>
+        <Grid item xs={3}></Grid>
+        <Grid item xs={6}>
+          <Typography color="secondary" variant="subtitle2">
+            Company Name Goes Here,<br/>
+            Something here to give the footer a purpose!<br/>
+            Company Number: 1234567890
+          </Typography>
+        </Grid>
+        <Grid item xs={3}></Grid>
+      </Grid>
+      <Grid item xs={3}>
+        <List component="nav" aria-label="legal">
+          <ListItem button>
+            <ListItemIcon>
+              <SubjectIcon />
+            </ListItemIcon>
+            <ListItemText secondary="Terms and Conditions" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <LockIcon />
+            </ListItemIcon>
+            <ListItemText secondary="Privacy Policy" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <EmojiFoodBeverage />
+            </ListItemIcon>
+            <ListItemText secondary="Cookies Policy" />
+          </ListItem>
+        </List>
+      </Grid>
+      <Grid item xs={3}>
+        <List component="nav" aria-label="social">
+          <ListItem button>
+            <ListItemIcon>
+              <TwitterIcon />
+            </ListItemIcon>
+            <ListItemText secondary="Twitter" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <FacebookIcon />
+            </ListItemIcon>
+            <ListItemText secondary="Facebook" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <InstagramIcon />
+            </ListItemIcon>
+            <ListItemText secondary="Instagram" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <YouTubeIcon />
+            </ListItemIcon>
+            <ListItemText secondary="YouTube" />
+          </ListItem>
+        </List>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Footer;
