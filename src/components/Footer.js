f2e206c7e318e171e56f488f59f3ab748461fe13
programarivm <info@programarivm.com>
2021-02-14 12:20:49 +0100

    Added CookieConsent component to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,7 @@ import SubjectIcon from '@material-ui/icons/SubjectTwoTone';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import Typography from '@material-ui/core/Typography';
 import Link from 'next/link';
+import CookieConsent from 'react-cookie-consent';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -25,58 +26,63 @@ const Footer = () => {
   const classes = useStyles();
 
   return (
-    <Grid container className={classes.container}>
-      <Grid item xs={12} sm={12} md={1} />
-      <Grid item xs={12} sm={12} md={5} className={classes.company}>
-        <Typography color="secondary" variant="subtitle2">
-          HTML Form Builder,<br/>
-          Something here to give the footer a purpose!<br/>
-          Company Number: 1234567890
-        </Typography>
-      </Grid>
-      <Grid item xs={12} sm={12} md={3}>
-        <List component="nav" aria-label="legal">
-          <ListItem button>
-            <ListItemIcon>
-              <SubjectIcon />
-            </ListItemIcon>
-            <ListItemText secondary="Terms and Conditions" />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <LockIcon />
-            </ListItemIcon>
-            <ListItemText secondary="Privacy Policy" />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <EmojiFoodBeverage />
-            </ListItemIcon>
-            <ListItemText secondary="Cookies Policy" />
-          </ListItem>
-        </List>
-      </Grid>
-      <Grid item xs={12} sm={12} md={3}>
-        <List component="nav" aria-label="social">
-          <Link href="https://github.com/programarivm/formbuilder">
+    <>
+      <Grid container className={classes.container}>
+        <Grid item xs={12} sm={12} md={1} />
+        <Grid item xs={12} sm={12} md={5} className={classes.company}>
+          <Typography color="secondary" variant="subtitle2">
+            HTML Form Builder,<br/>
+            Something here to give the footer a purpose!<br/>
+            Company Number: 1234567890
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={12} md={3}>
+          <List component="nav" aria-label="legal">
             <ListItem button>
               <ListItemIcon>
-                <GitHubIcon />
+                <SubjectIcon />
               </ListItemIcon>
-              <ListItemText secondary="GitHub" />
+              <ListItemText secondary="Terms and Conditions" />
             </ListItem>
-          </Link>
-          <Link href="https://twitter.com/programarivm">
             <ListItem button>
               <ListItemIcon>
-                <TwitterIcon />
+                <LockIcon />
               </ListItemIcon>
-              <ListItemText secondary="Twitter" />
+              <ListItemText secondary="Privacy Policy" />
             </ListItem>
-          </Link>
-        </List>
+            <ListItem button>
+              <ListItemIcon>
+                <EmojiFoodBeverage />
+              </ListItemIcon>
+              <ListItemText secondary="Cookies Policy" />
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item xs={12} sm={12} md={3}>
+          <List component="nav" aria-label="social">
+            <Link href="https://github.com/programarivm/formbuilder">
+              <ListItem button>
+                <ListItemIcon>
+                  <GitHubIcon />
+                </ListItemIcon>
+                <ListItemText secondary="GitHub" />
+              </ListItem>
+            </Link>
+            <Link href="https://twitter.com/programarivm">
+              <ListItem button>
+                <ListItemIcon>
+                  <TwitterIcon />
+                </ListItemIcon>
+                <ListItemText secondary="Twitter" />
+              </ListItem>
+            </Link>
+          </List>
+        </Grid>
       </Grid>
-    </Grid>
+      <CookieConsent>
+        Cookies? Mmm, they are delicious! This website does not collect personal data through third-party cookies.
+      </CookieConsent>
+    </>
   );
 }
 
