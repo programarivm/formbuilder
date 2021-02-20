742f2a62fd1764d2d64155e475bb75ee1d1610a9
programarivm <info@programarivm.com>
2021-02-20 18:04:37 +0100

    Cleaned up the 'Add Textarea' dialog box

--- src/components/Dialog/Add/Textarea.js
+++ src/components/Dialog/Add/Textarea.js
@@ -3,12 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { increaseCount } from "actions/counterActions";
 import { add as addTextarea, cancel as cancelTextarea } from "actions/textareaActions";
 import htmlTagTypes from 'constants/htmlTag/Types';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@material-ui/core';
 
 export default function Textarea() {
   const initialState = {
